27bd6f7d3b5e5446a1e22b61bbbb46ccf5739860
Tommaso Todeschini <tommaso.todeschini05@gmail.com>
2023-01-30 09:55:03 +0100

    alimenti e cancelleria

--- Esercitazione/Alimentare.cs
+++ Esercitazione/Alimentare.cs
@@ -8,10 +8,65 @@ namespace Esercitazione
 {
     public class Alimentare : prodotto
     {
-        public DateTime scadenza;
-        public Alimentare(string id, string nome, string produttore, string descrizione, decimal prezzo, DateTime scadenza) : base(id, nome, produttore, descrizione, prezzo)
+        private DateTime _scadenza;
+        private decimal _scontato;
+        private string[] _ingredienti;
+
+        public Alimentare(string id, string nome, string prod, string descr, decimal prezzo, DateTime scadenza) : base(id, nome, prod, descr, prezzo)
         {
-            this.scadenza = scadenza;
+            Scadenza = scadenza;
+            Scontato = prezzo;
+            _ingredienti = new string[10];
+        }
+
+        public DateTime Scadenza
+        {
+            get
+            {
+                return _scadenza;
+            }
+            private set
+            {
+                if (value > DateTime.Today)
+                    _scadenza = value;
+                else
+                    throw new Exception("Inserire un codice valido");
+            }
+        }
+        public decimal Scontato
+        {
+            get
+            {
+                return _scontato;
+            }
+            set
+            {
+                if (value > 0)
+                    _scontato = value;
+                else
+                    throw new Exception("Il prezzo deve essere positivo");
+            }
+        }
+        public void ApplicaS()
+        {
+            if (DateTime.Today.DayOfWeek - Scadenza.DayOfWeek < 7)
+            {
+                Scontato = 0.5m * Prezzo;
+            }
+        }
+
+        public void Carica(string a, string b, string c, string d, string e, string f, string g, string h, string i, string l)
+        {
+            _ingredienti[0] = a;
+            _ingredienti[1] = b;
+            _ingredienti[2] = c;
+            _ingredienti[3] = d;
+            _ingredienti[4] = e;
+            _ingredienti[5] = f;
+            _ingredienti[6] = g;
+            _ingredienti[7] = h;
+            _ingredienti[8] = i;
+            _ingredienti[9] = l;
         }
 
     }
